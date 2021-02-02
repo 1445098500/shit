8f63d4a507f976337f1a90c69553102676c9045f
pcy.kingsoft <1445098500@qq.com>
2021-02-02 17:56:17 +0800

    20210202

--- leetcode/20210125/demo1.cpp
+++ leetcode/20210125/demo1.cpp
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <windows.h>
-int main()
-{
-    printf("Hello World\n");
-    system("pause");
-    return 0;
+int removeDuplicates(int* nums, int numsSize){
+    int j = numsSize;
+    if(j > 1){
+        j = 1;
+        for(int i = 1; i < numsSize; i ++){
+            if(nums[i] == nums[i - 1]){
+                continue;
+            } else{
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+    }
+    return j;
 }
