ccf764b633523c7806705d605d82cb164469a8cd
home <1445098500@qq.com>
2021-02-14 18:30:49 +0800

    new

---
+++ algorithm/src/test/java/Test.java
@@ -0,0 +1,22 @@
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("jack");
+        s1.setAge(11);
+
+        Student s2 = new Student();
+        s2.setName("rose");
+        s2.setAge(20);
+
+        Comparable max = getMax(s1,s2);
+        System.out.println(max);
+    }
+    public static Comparable getMax(Comparable c1,Comparable c2){
+        int result = c1.compareTo(c2);
+        if (result>=0){
+            return c1;
+        }else{
+            return c2;
+        }
+    }
+}
